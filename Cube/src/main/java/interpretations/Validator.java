7f16aed80b99b59fbaa50331e35f75dbfe34654a
Henipiter <henipiter@wp.pl>
2021-10-31 08:05:16 +0100

    add Validator

---
+++ Cube/src/main/java/interpretations/Validator.java
@@ -0,0 +1,45 @@
+package interpretations;
+
+import DTOs.SolutionBLD;
+import cubes.Cube3x3;
+import lombok.Getter;
+import methods.BLDs.BLD3X3;
+
+import java.util.ArrayList;
+
+@Getter
+public class Validator {
+
+    private boolean rollingPop;
+    private boolean ollParity;
+    private boolean pllParity;
+
+    private Cube3x3 cube3x3;
+
+    public Validator(Cube3x3 cube3x3) {
+        this.cube3x3 = cube3x3;
+        BLD3X3 bld3X3 = new BLD3X3(cube3x3);
+        ArrayList<SolutionBLD> solution = bld3X3.solve();
+        System.out.println(SolutionBLD.getWholeMarks(solution));
+        cube3x3.makeMoves(SolutionBLD.getWholeAlgBLD(solution));
+        validateRollingPop();
+        validateOllParity();
+        validatePllParity();
+    }
+
+    public void validateRollingPop() {
+        rollingPop = cube3x3.getCube()[0][0] != cube3x3.getCube()[0][2];
+    }
+
+    public void validateOllParity() {
+        ollParity = cube3x3.getCube()[0][4] != cube3x3.getCube()[0][0];
+    }
+
+    public void validatePllParity() {
+        BLD3X3 bld3X3 = new BLD3X3(cube3x3);
+        int vertexSolutionSize = bld3X3.solveAllVertices().size();
+        int edgeSolutionSize = bld3X3.solveAllEdges().size();
+        pllParity = vertexSolutionSize != edgeSolutionSize;
+    }
+
+}
